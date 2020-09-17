1c0e15cc821784895a3b2d64c398b87405526ac4
manishverma10 <bsp.manishverma@gmail.com>
2020-09-17 19:47:52 +0530

    djano react todo-app

---
+++ ui/build/precache-manifest.4f97b50c075ed9ae5ef6581c405345b7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "331c7bfaac357d36b3929971d4bbb42b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0080345540d424221e1b",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "5869bcf491ae47ed00d5",
+    "url": "/static/css/main.46d6f905.chunk.css"
+  },
+  {
+    "revision": "0080345540d424221e1b",
+    "url": "/static/js/2.85f88638.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.85f88638.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5869bcf491ae47ed00d5",
+    "url": "/static/js/main.9879e376.chunk.js"
+  },
+  {
+    "revision": "c2159e35e941e5b77ed4",
+    "url": "/static/js/runtime-main.358d1757.js"
+  }
+]);
